4a903a6a48feb5c349f81827c2d34d0d0aa136d1
unknown <ivanvarelas01@gmail.com>
2019-10-01 18:24:22 +0200

    ivan

--- smr2b.php
+++ smr2b.php
@@ -2,10 +2,25 @@
 Ivan
  <a href="http://192.168.0.177/smr2b_jesus/smr2b.php">jesus</a>
 
+
+<form>
+<input name = "email" type = "email">
+<button> email </button>
+</form> 
+
 <?php
 $f=fopen('visitas.txt','a');
 fwrite($f,date('Y-m-d H:i:s '));
 fwrite($f,$_SERVER['REMOTE_ADDR']);
 fwrite($f,"\r\n");
 fclose($f)
+?>
+
+<?php
+if(isset($_GET['email'])){
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['email']);
+	fwrite($f,"\r\n");
+	fclose($f);
+}
 ?>
