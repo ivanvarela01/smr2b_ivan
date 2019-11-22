20af5c8813192d311601c7e71afc1dc7ae212342
unknown <ivanvarelas01@gmail.com>
2019-11-22 17:04:16 +0100

    ivan

---
+++ html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> iván </title>
+	</head>
+	<body>
+		<center>
+			<h1> HOLA <h1>
+		</center>
+		<h3>Hola, buenas tardes soy iván y me gusta el <a href="https://es.wikipedia.org/wiki/Baloncesto">baloncesto</a>.<h3>
+		<center>
+		<h2><b><u> <font color="blue"> <font size="5"> El Baloncesto </h2></b></u></font></font>
+		</center>
+		<font side="3"> El baloncesto es un deporte de equipo, jugado entre dos conjuntos de cinco jugadores cada uno durante cuatro períodos o cuartos de diez o doce minutos cada uno. El objetivo del equipo es anotar puntos introduciendo un balón por la canasta, un aro a 3,05 metros sobre la superficie de la pista de juego del que cuelga una red.</font><br><br><br>
+		<center>
+		<img src="https://cdn.britannica.com/96/195196-050-3909D5BD/Michael-Jordan-1988.jpg" width="290">
+		</center>
+	</body>
+
+</html>
